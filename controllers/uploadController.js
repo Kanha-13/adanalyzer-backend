dd843c5ae199a549a9af3849872855d1ab7dd993
kanha-13 <kanha.agr11@gmail.com>
2025-01-28 20:54:06 +0530

    fix upload file error if no uploads folder in root path

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,10 +1,16 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+const uploadsDir = path.join(__dirname, '../uploads');
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
 
 // Multer configuration
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../uploads'));
+    cb(null, uploadsDir);
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}_${file.originalname}`);
